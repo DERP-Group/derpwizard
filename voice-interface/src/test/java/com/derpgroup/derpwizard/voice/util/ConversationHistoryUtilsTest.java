93f31615c7e5180bb9aa9f370e7579e1f204b466
Eric <galactoise@gmail.com>
2015-11-13 13:14:09 -0800

    Updating unit tests

--- voice-interface/src/test/java/com/derpgroup/derpwizard/voice/util/ConversationHistoryUtilsTest.java
+++ voice-interface/src/test/java/com/derpgroup/derpwizard/voice/util/ConversationHistoryUtilsTest.java
@@ -1,51 +1,66 @@
 package com.derpgroup.derpwizard.voice.util;
 
 import java.util.ArrayDeque;
-import java.util.HashMap;
+import java.util.Deque;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-import static org.mockito.Mockito.when;
-
+import com.amazon.speech.slu.Intent;
+import com.amazon.speech.speechlet.IntentRequest;
 import com.derpgroup.derpwizard.voice.model.AlexaInput;
 import com.derpgroup.derpwizard.voice.model.CommonMetadata;
 import com.derpgroup.derpwizard.voice.model.ConversationHistoryEntry;
-import com.derpgroup.derpwizard.voice.model.VoiceInput.MessageType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@RunWith(MockitoJUnitRunner.class)
 public class ConversationHistoryUtilsTest {
-
-  @Mock AlexaInput mockAlexaInput;
-  @Mock CommonMetadata mockCommonMetadata;
   
   ObjectMapper mapper;
   
   @Before
   public void setup(){
     
-    when(mockCommonMetadata.getConversationHistory()).thenReturn(new ArrayDeque<ConversationHistoryEntry>());
-    
-    when(mockAlexaInput.getMessageSubject()).thenReturn("subject");
-    when(mockAlexaInput.getMessageType()).thenReturn(MessageType.DEFAULT);
-    when(mockAlexaInput.getMessageAsMap()).thenReturn(new HashMap<String,String>());
-    
     mapper = new ObjectMapper();
   }
   
-  /*@Test
+  @Test
   public void testAppendToConversationHistory_firstEntry(){
-    ConversationHistoryUtils.appendToConversationHistory(mockAlexaInput, new CommonMetadata(), mapper);
-    assertNotNull(mockAlexaInput);
-    assertNotNull(mockAlexaInput.getMetadata());
-    assertNotNull(mockAlexaInput.getMetadata().getConversationHistory());
-    assertEquals(1, mockAlexaInput.getMetadata().getConversationHistory().size());
-  }*/
+    
+    IntentRequest intentRequest = IntentRequest.builder().withRequestId("123").withIntent(Intent.builder().withName("testSubject").build()).build();
+    AlexaInput alexaInput = new AlexaInput(intentRequest);
+    
+    ConversationHistoryUtils.appendToConversationHistory(alexaInput, alexaInput.getMetadata(), mapper);
+    assertNotNull(alexaInput);
+    assertNotNull(alexaInput.getMetadata());
+    assertNotNull(alexaInput.getMetadata().getConversationHistory());
+    assertEquals(1, alexaInput.getMetadata().getConversationHistory().size());
+    assertNotNull(alexaInput.getMetadata().getConversationHistory().peek());
+    assertEquals("testSubject",alexaInput.getMetadata().getConversationHistory().pop().getMessageSubject());
+  }
+  
+  @Test
+  public void testAppendToConversationHistory_existingEntry(){
+    CommonMetadata commonMetadata = new CommonMetadata();
+    ConversationHistoryEntry entry = new ConversationHistoryEntry();
+    entry.setMessageSubject("testSubject1");
+    Deque<ConversationHistoryEntry> conversationHistory = new ArrayDeque<ConversationHistoryEntry>();
+    conversationHistory.push(entry);
+    commonMetadata.setConversationHistory(conversationHistory);
+    
+    IntentRequest intentRequest = IntentRequest.builder().withRequestId("123").withIntent(Intent.builder().withName("testSubject2").build()).build();
+    AlexaInput alexaInput = new AlexaInput(intentRequest, commonMetadata);
+    
+    ConversationHistoryUtils.appendToConversationHistory(alexaInput, alexaInput.getMetadata(), mapper);
+    assertNotNull(alexaInput);
+    assertNotNull(alexaInput.getMetadata());
+    assertNotNull(alexaInput.getMetadata().getConversationHistory());
+    assertEquals(2, alexaInput.getMetadata().getConversationHistory().size());
+    assertNotNull(alexaInput.getMetadata().getConversationHistory().peek());
+    assertEquals("testSubject2",alexaInput.getMetadata().getConversationHistory().pop().getMessageSubject());
+    assertNotNull(alexaInput.getMetadata().getConversationHistory().peek());
+    assertEquals("testSubject1",alexaInput.getMetadata().getConversationHistory().pop().getMessageSubject());
+  }
 }
